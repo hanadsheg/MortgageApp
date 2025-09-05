34551effd3a9170cbbb7eecdfdb8dac6fc63cadb
hanad <hanadshegowbusiness@gmail.com>
2025-09-05 19:42:36 +0100

    remove broken image from contact page

--- src/components/ContactTitle.jsx
+++ src/components/ContactTitle.jsx
@@ -11,11 +11,7 @@ function ContactTitle() {
           <p>We're here to help, this page will give you all the useful telephone numbers you need</p>
         </div>
 
-        <div className = "contact-info">
-            <a href = "https://x.com/LloydsBank?WT.ac=NavBarBottom/Navigation/Asklloyd?url=https%3A%2F%2Fprod-lloyds.adobecqms.net%2F.html&text=Personal&description=Wherever%20you%20want%20to%20get%20to%20in%20life%2C%20Lloyds%20Bank%20has%20a%20range%20of%20bank%20accounts%20and%20personal%20banking%20services%20to%20suit%20you.%20Visit%20us%20today%20to%20find%20out%20more">
-              <img className = "contact-icon" src = "../assets/xphoto.png" alt="Contact Icon"/>
-            </a>
-        </div>
+        
 
       <img className="contact-photo" src={ContactPhoto} alt="Contact" />
 
